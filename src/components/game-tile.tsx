47bbd3b4d742c0a6c84524c807ca6e009fe49317
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-20 15:53:20 +1000

    fix: remove unused code from game-tile.tsx

--- src/components/game-tile.tsx
+++ src/components/game-tile.tsx
@@ -1,19 +1,19 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-
-export default function GameTile({ content, handleClick }: {
-  content: string,
-  handleClick: () => void,
-
+export default function GameTile({
+  content,
+  handleClick,
+}: {
+  content: string;
+  handleClick: () => void;
 }) {
-  const [tileContent, setTileContent] = useState("");
   console.log(content);
   return (
-    <div className="bg-blue-200 rounded aspect-square flex items-center justify-center h-[25vh] hover:bg-blue-400 cursor-pointer" onClick={handleClick}>
-      {
-        content === "x" ? "x" : content === "o" ? "o" : ""
-      }
+    <div
+      className="bg-blue-200 rounded aspect-square flex items-center justify-center h-[25vh] hover:bg-blue-400 cursor-pointer"
+      onClick={handleClick}
+    >
+      {content === "x" ? "x" : content === "o" ? "o" : ""}
     </div>
-  )
+  );
 }
