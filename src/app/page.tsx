2bb01676be27808484ff4b5f4f18df8d0b371823
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-20 14:58:45 +1000

    feat: add gameboard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Gameboard from "@/components/gameboard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Gameboard />
     </main>
   );
 }
---
+++ src/components/gameboard.tsx
@@ -0,0 +1,7 @@
+export default function Gameboard() {
+  return (
+    <div className="aspect-square grid grid-cols-3 border-2">
+      
+    </div>
+  )
+}
