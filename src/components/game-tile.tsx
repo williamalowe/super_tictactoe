3ee957cd49676afdc75c3d635d8ef393c6c32595
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-20 15:59:29 +1000

    feat: add xMarker component

--- src/components/game-tile.tsx
+++ src/components/game-tile.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import XMarker from "./xMarker";
+
 export default function GameTile({
   content,
   handleClick,
@@ -12,7 +14,7 @@ export default function GameTile({
       className="bg-blue-200 rounded aspect-square flex items-center justify-center h-[25vh] hover:bg-blue-400 cursor-pointer"
       onClick={handleClick}
     >
-      {content === "x" ? "x" : content === "o" ? "o" : ""}
+      {content === "x" ? <XMarker /> : content === "o" ? "o" : ""}
     </div>
   );
 }
---
+++ src/components/xMarker.tsx
@@ -0,0 +1,9 @@
+
+export default function XMarker() {
+  return (
+    <div className="relative">
+      <div className="absolute w-[20vh] h-1 bg-black rotate-45 -left-[10vh] rounded"/>
+      <div className="absolute w-[20vh] h-1 bg-black -rotate-45 -left-[10vh] rounded"/>
+    </div>
+  )
+}
