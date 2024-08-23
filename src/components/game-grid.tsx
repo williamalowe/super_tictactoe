eec4d2fc66f9469f31805ae28f5df27ec6479450
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-23 15:58:41 +1000

    styles: add grid-styles

--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -2,7 +2,7 @@ import GameTile from "./game-tile";
 
 export default function GameGrid() {
   return (
-    <div className="flex-1 aspect-square grid grid-cols-3 gap-2 bg-red-600 p-2">
+    <div className="flex-1 aspect-square grid grid-cols-3 gap-2 p-2 bg-white">
       <GameTile />
       <GameTile />
       <GameTile />
--- src/components/gameboard.tsx
+++ src/components/gameboard.tsx
@@ -2,7 +2,7 @@ import GameGrid from "./game-grid";
 
 export default function Gameboard() {
   return (
-    <section className="flex-1 aspect-square flex flex-col gap-4">
+    <section className="flex-1 aspect-square flex flex-col gap-4 bg-black">
       <div className="flex-1 flex gap-4">
         <GameGrid />
         <GameGrid />
