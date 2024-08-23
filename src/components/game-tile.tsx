291430ad1a02b72dc3284f560423dd20157d9ef8
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-23 16:46:46 +1000

    feat: add oToken component

--- src/components/game-tile.tsx
+++ src/components/game-tile.tsx
@@ -1,3 +1,4 @@
+import OToken from "./oToken"
 import XToken from "./xToken"
 
 export default function GameTile({ content }: {
@@ -9,7 +10,11 @@ export default function GameTile({ content }: {
         content === "x" ?
         <XToken />
         :
+        content === "o" ?
+        <OToken />
+        :
         <></>
+
       }
     </div>
   )
---
+++ src/components/oToken.tsx
@@ -0,0 +1,5 @@
+export default function OToken() {
+  return (
+    <div  className="flex-1 aspect-square rounded-full border-8 border-sky-600"/>
+  )
+}
