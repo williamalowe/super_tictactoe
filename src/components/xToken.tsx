bd7693fe777950170ebac7eaf3999506fa96fe43
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-23 16:43:10 +1000

    styles: update xToken component coloring

--- src/components/xToken.tsx
+++ src/components/xToken.tsx
@@ -1,8 +1,8 @@
 export default function XToken() {
   return (
     <div className="relative flex-1 flex items-center justify-center">
-      <div className="absolute w-full h-2 bg-black rotate-45"/>
-      <div className="absolute w-full h-2 bg-black -rotate-45"/>
+      <div className="absolute w-full h-2 bg-rose-600 rotate-45"/>
+      <div className="absolute w-full h-2 bg-rose-600 -rotate-45"/>
     </div>
   )
 }
