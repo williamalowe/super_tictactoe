7beaabe12bc90e4535eb8c5eeb3d70116b9ec999
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-23 16:34:30 +1000

    feat: add game-grid context associations

--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -1,17 +1,44 @@
 import GameTile from "./game-tile";
 
-export default function GameGrid() {
+type gameValues = {
+  game: number;
+  winner: string;
+  active: boolean;
+  contents: string[];
+};
+
+export default function GameGrid({ game }: { game: gameValues }) {
+
+  console.log(game);
   return (
     <div className="flex-1 aspect-square grid grid-cols-3 gap-2 p-2 bg-white">
-      <GameTile />
-      <GameTile />
-      <GameTile />
-      <GameTile />
-      <GameTile />
-      <GameTile />
-      <GameTile />
-      <GameTile />
-      <GameTile />
+      <GameTile 
+        
+      />
+      <GameTile 
+        
+      />
+      <GameTile 
+        
+      />
+      <GameTile 
+        
+      />
+      <GameTile 
+        
+      />
+      <GameTile 
+        
+      />
+      <GameTile 
+        
+      />
+      <GameTile 
+        
+      />
+      <GameTile 
+        
+      />
     </div>
-  )
+  );
 }
--- src/components/gameboard.tsx
+++ src/components/gameboard.tsx
@@ -2,26 +2,32 @@
 import { useGame } from "@/context/game-context";
 import GameGrid from "./game-grid";
 
+type gameValues = {
+  game: number;
+  winner: string;
+  active: boolean;
+  contents: string[];
+};
+
 export default function Gameboard() {
   const { games } = useGame();
 
-  console.log(games);
   return (
     <section className="flex-1 aspect-square flex flex-col gap-4 bg-black">
       <div className="flex-1 flex gap-4">
-        <GameGrid />
-        <GameGrid />
-        <GameGrid />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 1)} />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 2)} />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 3)} />
       </div>
       <div className="flex-1 flex gap-4">
-        <GameGrid />
-        <GameGrid />
-        <GameGrid />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 4)} />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 5)} />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 6)} />
       </div>
       <div className="flex-1 flex gap-4">
-        <GameGrid />
-        <GameGrid />
-        <GameGrid />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 7)} />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 8)} />
+        <GameGrid game={games.filter((game: gameValues) => game.game === 9)} />
       </div>
     </section>
   );
