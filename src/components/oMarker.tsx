e846183af92cc3469a7241677485556c13619e63
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-08-23 15:22:01 +1000

    init: remove old files

--- src/components/oMarker.tsx
+++
@@ -1,5 +0,0 @@
-export default function OMarker() {
-  return (
-    <div className="aspect-square border-4 rounded-full w-[7vh] border-red-600" />
-  );
-}
--- src/components/xMarker.tsx
+++
@@ -1,8 +0,0 @@
-export default function XMarker() {
-  return (
-    <div className="relative">
-      <div className="absolute w-[8vh] h-1 bg-blue-600 rotate-45 -left-[4vh] rounded" />
-      <div className="absolute w-[8vh] h-1 bg-blue-600 -rotate-45 -left-[4vh] rounded" />
-    </div>
-  );
-}
